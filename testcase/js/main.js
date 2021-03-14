a80bdd3684a66f542cd16456ab54ebc5184c8736
oleg-tgn <stelmah.taganrog@gmail.com>
2021-03-14 23:57:06 +0300

    processing shopping cart data and styles for mobile/tablet

--- testcase/js/main.js
+++ testcase/js/main.js
@@ -7,7 +7,7 @@ var app = new Vue({
               img: "img/jacket.png",
               name: "Amet minim mollit non deserunt ullamco est sit",
               count: 1,
-              price: 525
+              price: 525,              
           },
           {
             img: "img/sneakers.png",
@@ -16,15 +16,56 @@ var app = new Vue({
             price: 525
         }
       ],
+      paymethod: "credit_card",
+      promocode: "",      
       tax: 100,
       shipping: 150,
       discount: 0,
-      subTotal: 0
+      subTotal: 0,
+      maxCount: 100,
+      isSidebarVisibleMobile: false
     },
     methods: {
         deleteItem: function(index) {
-            this.basket.splice(index, 1);
-        }       
+            if (window.confirm('You really want to delete "' + this.basket[index].name + '"?'))
+                this.basket.splice(index, 1);
+        },
+        minusCount: function(index) {
+            this.basket[index].count--;
+            if (this.basket[index].count <= 0) {
+                if (window.confirm('You really want to delete "' + this.basket[index].name + '"?'))
+                    this.basket.splice(index, 1);
+                else
+                    this.basket[index].count = 1;            
+            }                   
+        },
+        plusCount: function(index) {
+            this.basket[index].count++;
+            if (this.basket[index].count > this.maxCount) {
+                alert("Sorry, but you cannot order more than "+ this.maxCount + " items");
+                this.basket[index].count = 100;
+            }
+        },
+        applyPromocode: function() {
+            if (this.promocode === "PROMO") {
+                this.discount = 25;
+            } else {
+                alert("Promocode is not valid");
+            }
+        },
+        checkCount: function(index) {
+            if (this.basket[index].count <= 0) {
+                if (window.confirm('You really want to delete "' + this.basket[index].name + '"?'))
+                    this.basket.splice(index, 1);
+                else
+                    this.basket[index].count = 1;            
+            }      
+
+            if (this.basket[index].count > this.maxCount) {
+                alert("Sorry, but you cannot order more than "+ this.maxCount + " items");
+                this.basket[index].count = 100;
+            }
+        }
     },
     computed: {
         getSubtotal: function() {
@@ -33,13 +74,11 @@ var app = new Vue({
                 sum += this.basket[i].price * this.basket[i].count;                
             }   
             this.subTotal = sum;
-            return sum;
+            return sum.toLocaleString('ru-RU');
         },
         getTotal: function() {
-            return this.subTotal + this.tax + this.shipping + this.discount;
-        }
-    },
-    filters: {
-        format: val => `${val}`.replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 '),
+            return (this.subTotal + this.tax + this.shipping - this.discount).toLocaleString('ru-RU');
+        },
+        
     },
   })
